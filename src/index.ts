bb811ead51b5b3ad1dee7c83cfc2b63c10288176
thatmarc_ <makunomark@gmail.com>
2018-02-13 21:24:55 +0300

    Sending push stk

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export default class WovenPay {
   private url: string
+  private authToken: string
 
   constructor(url: string) {
     this.url = url
@@ -12,6 +13,33 @@ export default class WovenPay {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ email: email, password: password })
+    })
+  }
+
+  setAuthToken(authToken: string) {
+    this.authToken = authToken
+  }
+
+  chargePayment(methodOfPayment: string, amount: number, mobile: string, customerEmail: string, orderDescription: string, reference: string) {
+    return fetch(this.url + '/payments/actions/charge', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(
+        {
+          "method": methodOfPayment,
+          "amount": amount,
+          "mobile": mobile,
+          "customer": {
+            "email": customerEmail
+          },
+          "order": {
+            "description": orderDescription
+          },
+          "reference": reference
+        }
+      )
     })
   }
 }
