b1a333855918c78699f7d2266b303f85bc9fd8e8
makunomark <makunomark@gmail.com>
2018-02-15 23:13:42 +0300

    Plan create, delete, edit, partial edit Webhook create, delete Payment charge, checkstatus Tests

--- src/index.ts
+++ src/index.ts
@@ -55,18 +55,106 @@ export default class WovenPay {
       },
       body: JSON.stringify(
         {
-          "method": methodOfPayment,
-          "amount": amount,
-          "mobile": mobile,
-          "customer": {
-            "email": customerEmail
+          'method': methodOfPayment,
+          'amount': amount,
+          'mobile': mobile,
+          'customer': {
+            'email': customerEmail
           },
-          "order": {
-            "description": orderDescription
+          'order': {
+            'description': orderDescription
           },
-          "reference": reference
+          'reference': reference
         }
       )
+    })
+  }
+
+  checkPaymentStatus(transactionId: string) {
+    return fetch(this.url + `/payments/${transactionId}/status`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'XPAY': `${this.apikey}:${this.apisecret}`,
+      }
+    })
+  }
+
+  createWebhook(event: string, target: string, key: string) {
+    return fetch(this.url + `/webhooks/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${this.authToken}`
+      },
+      body: JSON.stringify(
+        {
+          'event': event,
+          'target': target,
+          'key': key
+        })
+    })
+  }
+
+  deleteWebhook(webhookId: string) {
+    return fetch(`${this.url}/webhooks/${webhookId}/`, {
+      method: 'DELETE',
+      headers: {
+        'Authorization': `Token ${this.authToken}`
+      }
+    })
+  }
+
+  createPlan(name: string, price: number, business: string) {
+    return fetch(`${this.url}/plans/`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${this.authToken}`
+      },
+      body: JSON.stringify({
+        'name': name,
+        'price': price,
+        'business': business
+      })
+    })
+  }
+
+  editPlanName(planId: string, name: string) {
+    return fetch(`${this.url}/plans/${planId}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${this.authToken}`
+      },
+      body: JSON.stringify({
+        'name': name,
+      })
+    })
+  }
+
+  editPlan(planId: string, name: string, price: number, business: string) {
+    return fetch(`${this.url}/plans/${planId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${this.authToken}`
+      },
+      body: JSON.stringify({
+        'name': name,
+        'price': price,
+        'business': business
+      })
+    })
+  }
+
+  deletePlan(planId: string) {
+    return fetch(`${this.url}/plans/${planId}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Token ${this.authToken}`
+      }
     })
   }
 }
--- tests/test.js
+++ tests/test.js
@@ -11,7 +11,7 @@ const email = "jk@test.com"
 const password = "test12345"
 const wrongPassword = "test123451"
 
-describe('WovenPay', () => {
+describe('WovenPay JS SDK', () => {
   it('Initialize WovenPay object', () => {
     let wovenPay = new WovenPay(url, apikey, apisecret)
     expect(wovenPay).to.be.an.instanceof(WovenPay)
@@ -45,9 +45,82 @@ describe('WovenPay', () => {
     expect(response.status).to.be.equal(200)
   }).timeout(0)
 
-  it('Charge payment (Without token)', async () => {
+  it('Charge payment', async () => {
     let wovenPay = new WovenPay(url, apikey, apisecret)
     let chargePayment = await wovenPay.chargePayment('mobile.mpesa', 200, '254727383066', 'email@sample.com', 'desc', 'ref')
     expect(chargePayment.status).to.be.equal(200)
+  }).timeout(0)
+
+  it('Check payments status: Expect to fail', async () => {
+    let wovenPay = new WovenPay(url, apikey, apisecret)
+    let chargePayment = await wovenPay.chargePayment('mobile.mpesa', 200, '254727383066', 'email@sample.com', 'desc', 'ref')
+    let json = await chargePayment.json()
+    let response = await wovenPay.checkPaymentStatus(json.transaction_id)
+    expect(response.status).to.be.equal(400)
+  }).timeout(0)
+
+  it('Create webhook', async () => {
+    let wovenPay = new WovenPay(url, apikey, apisecret)
+    let token = await wovenPay.getAuthToken(email, password)
+    let jsonToken = await token.json()
+    wovenPay.setAuthToken(jsonToken.token)
+    let response = await wovenPay.createWebhook('customer.created', 'https://127.0.0.1:67014/', 'test')
+    let responseJson = await response.json()
+    expect(response.status).to.be.equal(201)
+  }).timeout(0)
+
+  it('Delete webhook', async () => {
+    let wovenPay = new WovenPay(url, apikey, apisecret)
+    let token = await wovenPay.getAuthToken(email, password)
+    let jsonToken = await token.json()
+    wovenPay.setAuthToken(jsonToken.token)
+    let response = await wovenPay.createWebhook('customer.created', 'https://127.0.0.1:67025/', 'test')
+    let responseJson = await response.json()
+    let response2 = await wovenPay.deleteWebhook(responseJson.id)
+    expect(response2.status).to.be.equal(204)
+  }).timeout(0)
+
+  it('Create plan', async () => {
+    let wovenPay = new WovenPay(url, apikey, apisecret)
+    let token = await wovenPay.getAuthToken(email, password)
+    let jsonToken = await token.json()
+    wovenPay.setAuthToken(jsonToken.token)
+    let response = await wovenPay.createPlan('customerpp', 200, 'bus_nRYFanSePwq5pHP4n5Nza3')
+    expect(response.status).to.be.equal(201)
+  }).timeout(0)
+
+  it('Edit plan name', async () => {
+    let wovenPay = new WovenPay(url, apikey, apisecret)
+    let token = await wovenPay.getAuthToken(email, password)
+    let jsonToken = await token.json()
+    wovenPay.setAuthToken(jsonToken.token)
+    let response = await wovenPay.createPlan('customeroo', 200, 'bus_nRYFanSePwq5pHP4n5Nza3')
+    let jsonResponse = await response.json()
+    let response2 = await wovenPay.editPlanName(jsonResponse.id, 'customer9')
+    expect(response2.status).to.be.equal(200)
+  }).timeout(0)
+
+  it('Edit plan fully', async () => {
+    let wovenPay = new WovenPay(url, apikey, apisecret)
+    let token = await wovenPay.getAuthToken(email, password)
+    let jsonToken = await token.json()
+    wovenPay.setAuthToken(jsonToken.token)
+    let response = await wovenPay.createPlan('cuhffg', 200, 'bus_nRYFanSePwq5pHP4n5Nza3')
+    let jsonResponse = await response.json()
+    console.log(JSON.stringify(jsonResponse))
+    let response2 = await wovenPay.editPlan(jsonResponse.id, 'PNsahm5', 400, 'bus_nRYFanSePwq5pHP4n5Nza3')
+    console.log(JSON.stringify(await response2.json()))
+    expect(response2.status).to.be.equal(200)
+  }).timeout(0)
+
+  it('Delete plan', async () => {
+    let wovenPay = new WovenPay(url, apikey, apisecret)
+    let token = await wovenPay.getAuthToken(email, password)
+    let jsonToken = await token.json()
+    wovenPay.setAuthToken(jsonToken.token)
+    let response = await wovenPay.createPlan('customercc', 200, 'bus_nRYFanSePwq5pHP4n5Nza3')
+    let jsonResponse = await response.json()
+    let response2 = await wovenPay.deletePlan(jsonResponse.id)
+    expect(response2.status).to.be.equal(204)
   }).timeout(0)
 })
